50988ec3ac06c0c6119985ddcf56c1f378f431ed
Stephen <stevobialor@gmail.com>
2022-12-13 16:51:11 +0000

    done

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
-	if (new = NULL)
+	if (new == NULL)
 		return (NULL);
 	new->left = NULL;
 	new->right = NULL;
--- binary_trees.h
+++ binary_trees.h
@@ -13,7 +13,7 @@
  */
 struct binary_tree_s
 {
-   	int n;
+	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
