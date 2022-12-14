8b460b3cab8bd8531082d9354edbb81fec3832f2
Stephen <stevobialor@gmail.com>
2022-12-14 06:53:24 +0000

    done

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,7 +10,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t l = 0;
 	size_t r = 0;
 	size_t s = 0;
-
+	if (tree == NULL)
+		return (0);
 	l = (tree && tree->left) ? binary_tree_size(tree->left) : 0;
 	r = (tree && tree->right) ? binary_tree_size(tree->right) : 0;
 	s = l + r + 1;
