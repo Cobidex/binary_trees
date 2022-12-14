261966948f74620de4c410ece9293be43d4e0dbe
Stephen <stevobialor@gmail.com>
2022-12-14 20:01:44 +0000

    done

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,6 +11,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	n = binary_tree_size(tree) - binary_tree_leaves(tree);
+	n += ((tree->left || tree->right) ? 1 : 0);
+	n += binary_tree_nodes(tree->left);
+	n += binary_tree_nodes(tree->right);
 	return (n);
 }
