189c598afbc26a6c1fcde3c3ed5ae1dc60798825
Stephen <stevobialor@gmail.com>
2022-12-14 16:18:18 +0000

    done

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,10 +8,16 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t n = 0;
+	size_t nr = 0;
+	size_t nl = 0;
 	const binary_tree_t *l = tree->left;
 	const binary_tree_t *r = tree->right;
 
 	if (tree)
-		n = (l && r) ? binary_tree_leaves(tree->left) : 1;
+	{
+		nl = (l && r) ? binary_tree_leaves(tree->left) : 1;
+		nr = (l && r) ? binary_tree_leaves(tree->right) : 1;
+	}
+	n = nl + nr;
 	return (n);
 }
